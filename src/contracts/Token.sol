2d2989948a490f37ce6404fcb31a4d117f9975c7
Marcin Krol <marcin80krol@gmail.com>
2021-06-15 17:16:54 +0100

    TOKEN UPDATE

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
+
     using SafeMath for uint;
 
     string public name = "DApp Token";
@@ -11,7 +12,8 @@ contract Token {
     uint256 public totalSupply;
     // track balances
     mapping(address => uint256) public balanceOf;
-    // send balances
+    // events
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor() public {
         totalSupply = 1000000 * (10 ** decimals);
@@ -19,9 +21,14 @@ contract Token {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
+
+    
 
 }
