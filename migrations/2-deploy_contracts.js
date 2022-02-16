43d12a95336975d0f85f7677ee71ec18d88aef23
Marcin Krol <junic@wp.pl>
2022-02-16 15:55:18 +0000

    Contract deployment to Kovan network

--- migrations/2-deploy_contracts.js
+++ migrations/2-deploy_contracts.js
@@ -1,8 +1,8 @@
 const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
-module.exports = async function (deployer) {
-  const accounts = await web3.eth.getAccounts();
+module.exports = async function (deployer, network, accounts) {
+  //const accounts = await web3.eth.getAccounts();
   const feeAccount = accounts[0];
   const feePercent = 10;
   
--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,31 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const privateKeys = process.env.PRIVATE_KEYS || "";
+const infuraKey = process.env.INFURA_API_KEY;
 
 module.exports = {
-
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
     },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKeys.split(','),   // array of account private keys
+          `https://kovan.infura.io/v3/${infuraKey}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 500000000,
+      network_id: 42
+    }
   },
-
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
-
-  // Configure your compilers
   compilers: {
     solc: {
       optimizer: {
